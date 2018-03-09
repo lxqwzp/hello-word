bb0507c53a689c04f16e3f3f998f657bbd5a9fd4
zp <516075396@qq.com>
2018-03-09 14:04:21 +0800

    第一次提交

---
+++ IO/IO/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace IO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(File.Exists(@"C:\HelloIO\IO.txt"));
+            Console.WriteLine(Directory.Exists(@"C:\"));
+
+            string path = ".";
+            if (args.Length > 0)
+            {
+                if (Directory.Exists(args[0]))
+                { path = args[0]; }
+                else
+                {
+                    Console.WriteLine("{0} not found;using current directory:", args[0]);
+                }
+            } 
+            DirectoryInfo dir = new DirectoryInfo(path);
+            foreach (FileInfo f in dir.GetFiles("*.exe"))
+            {
+                string name = f.Name;
+                long size = f.Length;
+                DateTime creationTime = f.CreationTime;
+                Console.WriteLine("{0, -12:N0}{1,-20:g}{2}",size,creationTime,name);
+            }
+            Console.Read();
+        }
+    }
+}
